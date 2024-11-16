8cc4f4e9c1e36a03fa1e0e7621d434e7b2a9e2d5
mbimbij <joseph.mbimbi@gmail.com>
2024-11-16 17:02:39 +0100

    Rename and reorganize existing tests

--- src/test/java/org/example/TennisApplicationShould.java
+++ src/test/java/org/example/TennisApplicationShould.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.assertj.core.api.ThrowableAssert;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -13,27 +14,30 @@ import java.io.PrintStream;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class TennisApplicationShould {
+class TennisApplicationTest {
 
+    private TennisApplication tennisApplication;
     private ByteArrayOutputStream baos;
 
     @BeforeEach
     void setUp() {
+        tennisApplication = new TennisApplication();
         baos = new ByteArrayOutputStream();
         System.setOut(new PrintStream(baos));
     }
 
-    @Test
-    @EnabledIfEnvironmentVariable(named = "OUTSIDE", matches = ".*")
-    void play_and_print_an_entire_game_without_deuces_and_advantages() {
-        // GIVEN
-        TennisApplication tennisApplication = new TennisApplication();
+    @Nested
+    class AcceptanceTests {
+        @Test
+        @EnabledIfEnvironmentVariable(named = "OUTSIDE", matches = ".*")
+        void should_play_and_print_an_entire_game_without_deuces_and_advantages() {
+            // GIVEN
 
-        // WHEN
-        tennisApplication.playGameForInput("ABABAA");
+            // WHEN
+            tennisApplication.playGameForInput("ABABAA");
 
-        // THEN
-        String expectedOutput = """
+            // THEN
+            String expectedOutput = """
                 Player A : 15 / Player B : 0
                 Player A : 15 / Player B : 15
                 Player A : 30 / Player B : 15
@@ -41,20 +45,20 @@ class TennisApplicationShould {
                 Player A : 40 / Player B : 30
                 Player A wins the game
                 """;
-        String actualOutput = baos.toString();
-        assertThat(actualOutput).isEqualTo(expectedOutput);
-    }
+            String actualOutput = baos.toString();
+            assertThat(actualOutput).isEqualTo(expectedOutput);
+        }
 
-    @Test()
-    @EnabledIfEnvironmentVariable(named = "OUTSIDE", matches = ".*")
-    void play_and_print_an_entire_game_with_deuces_and_advantages() {
-        // GIVEN
-        TennisApplication tennisApplication = new TennisApplication();
-        // WHEN
-        tennisApplication.playGameForInput("ABABAA");
+        @Test()
+        @EnabledIfEnvironmentVariable(named = "OUTSIDE", matches = ".*")
+        void should_play_and_print_an_entire_game_with_deuces_and_advantages() {
+            // GIVEN
+            TennisApplication tennisApplication = new TennisApplication();
+            // WHEN
+            tennisApplication.playGameForInput("ABABAA");
 
-        // THEN
-        String expectedOutput = """
+            // THEN
+            String expectedOutput = """
                 Player A : 15 / Player B : 0
                 Player A : 15 / Player B : 15
                 Player A : 30 / Player B : 15
@@ -68,49 +72,57 @@ class TennisApplicationShould {
                 advantage Player A
                 Player A wins the game
                 """;
-        String actualOutput = baos.toString();
-        assertThat(actualOutput).isEqualTo(expectedOutput);
+            String actualOutput = baos.toString();
+            assertThat(actualOutput).isEqualTo(expectedOutput);
+        }
     }
 
-    @ParameterizedTest()
-    @ValueSource(
-            strings = {
-                    "",
-                    " ",
-                    "\t \n"
-            }
-    )
-    void throw_an_error_on_blank_input(String input) {
-        // GIVEN
-        TennisApplication tennisApplication = new TennisApplication();
+    @Nested
+    public class ErrorCases{
+        @ParameterizedTest()
+        @ValueSource(
+                strings = {
+                        "",
+                        " ",
+                        "\t \n"
+                }
+        )
+        void should_throw_an_error_on_blank_input(String input) {
+            // GIVEN
+            TennisApplication tennisApplication = new TennisApplication();
 
-        // WHEN
-        ThrowableAssert.ThrowingCallable throwingCallable = () -> tennisApplication.playGameForInput(input);
+            // WHEN
+            ThrowableAssert.ThrowingCallable throwingCallable = () -> tennisApplication.playGameForInput(input);
 
-        // THEN
-        assertThatThrownBy(throwingCallable)
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("input shouldn't be blank");
+            // THEN
+            assertThatThrownBy(throwingCallable)
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("input shouldn't be blank");
+        }
+
+        @ParameterizedTest
+        @ValueSource(
+                strings = {
+                        "ABC",
+                        "AB AB",
+                        "AB AB"
+                }
+        )
+        void should_throw_an_error_on_invalid_character(String input) {
+            // GIVEN
+            TennisApplication tennisApplication = new TennisApplication();
+
+            // WHEN
+            ThrowableAssert.ThrowingCallable throwingCallable = () -> tennisApplication.playGameForInput(input);
+
+            // THEN
+            assertThatThrownBy(throwingCallable)
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("invalid character in input: %s".formatted(input));
+        }
     }
 
-    @ParameterizedTest
-    @ValueSource(
-            strings = {
-                    "ABC",
-                    "AB AB",
-                    "AB AB"
-            }
-    )
-    void throw_an_error_on_invalid_character(String input) {
-        // GIVEN
-        TennisApplication tennisApplication = new TennisApplication();
-
-        // WHEN
-        ThrowableAssert.ThrowingCallable throwingCallable = () -> tennisApplication.playGameForInput(input);
-
-        // THEN
-        assertThatThrownBy(throwingCallable)
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("invalid character in input: %s".formatted(input));
+    @Test
+    void should_print_1_action() {
     }
 }
